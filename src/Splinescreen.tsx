18c9c38d9f80e54664f6a032186c7e86e14beb49
Sai Srinivas <saisrinivas2511@gmail.com>
2024-01-19 11:35:03 +0530

    Added some basic information and assets

--- src/Splinescreen.tsx
+++ src/Splinescreen.tsx
@@ -16,19 +16,38 @@ const Splinescreen = () => {
 
   return (
     <div className="container">
-      
       <div className="left-section">
         <Spline
           scene="https://prod.spline.design/a6oYLRa6Ib62M7DP/scene.splinecode"
           onLoad={onLoad}
         />
-      </div><div className="right-section">
-        <h2>About</h2>
-        <p>This is the about section.</p>
+      </div>
+      <div className="right-section">
+        <div className="right-title">
+          <h2 style={{ paddingTop: "5vh" }}>Hello, I am Sai Srinivas S S</h2>
+          <img
+            style={{
+              margin: "15px",
+              width: "10vw",
+              height: "20vh",
+              objectFit: "cover",
+              borderRadius: 90,
+            }}
+            alt="wallpaper"
+            src={require("./Assets/Sai.heic")}
+          />
+        </div>
+
+        <p style={{ margin: 15, padding: 10 }}>
+          {" "}
+          A frontend developer with 1 year of experience in React-Native &
+          Reactjs. I have actively designed and coded Mobile and Web apps . In
+          addition to React-Native, I have expertise in Redux, unit testing
+          using jest, agile methodologies, Git, Firebase,GraphQL & many more.
+        </p>
       </div>
     </div>
   );
 };
 
 export default Splinescreen;
-
